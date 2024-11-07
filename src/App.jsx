715eb411f74ed608d85e5da17f9da60aff0ef381
Rid Van <fatzey2010@gmail.com>
2024-11-07 15:06:12 +0300

    unit_2_base

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 import "./app.css";
-import { useState } from "react";
-import { DarkModeContext } from "./DarkModeContext.js";
-import { MainComponent } from "./MainComponent.jsx";
-import { DarkModeProvider } from "./DarkModeProvider.jsx";
 
 export const App = () => {
-  return (
-    <DarkModeProvider>
-      <MainComponent />
-    </DarkModeProvider>
-  );
+  return <div>App</div>;
 };
--- src/useDarkMode.js
+++
@@ -1,6 +0,0 @@
-import { DarkModeContext } from "./DarkModeContext.js";
-import { useContextSelector } from "use-context-selector";
-
-export const useDarkMode = (selector) => {
-  return useContextSelector(DarkModeContext, selector);
-};
